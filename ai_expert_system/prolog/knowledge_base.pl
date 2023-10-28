691cd63cc29430922174e08366cf917e948b524b
Matthewsamuels-22 <113641022+Matthewsamuels-22@users.noreply.github.com>
2023-10-28 12:11:30 -0500

    Added knowledge_base.pl and added to prolog_controller.py

--- ai_expert_system/prolog/knowledge_base.pl
+++ ai_expert_system/prolog/knowledge_base.pl
@@ -1,8 +1,79 @@
-male('John')
-male('Mark')
-female('Kim')
-female('Alice')
+# Set the initial default GPA
+default_gpa(2.2).
 
-parent_of('Mark','John')
-parent_of('Kim','Alice')
+# GPA Calculation for Semester 1
+gpa(StudentID, 1, GPA) :-
+    write('Calculating GPA for Semester 1 for Student ID: '), write(StudentID), nl,
+    findall(Credits * GradePoints,
+            (module_details(Module, Year, 1, StudentID, GradePoints),
+             module_master(Module, Credits)),
+            CreditGradePointList),
 
+    # Display the CreditGradePointList
+    write('CreditGradePointList: '), write(CreditGradePointList), nl,
+
+    # Calculate the total grade points for Semester 1
+    sum_grade_points(CreditGradePointList, TotalGradePoints),
+
+    # Calculate the total credits for Semester 1
+    sum_credits(1, StudentID, TotalCredits),
+
+    # Display TotalGradePoints and TotalCredits
+    write('TotalGradePoints: '), write(TotalGradePoints), nl,
+    write('TotalCredits: '), write(TotalCredits), nl,
+
+    # Calculate GPA for Semester 1 as the ratio of TotalGradePoints to TotalCredits
+    (TotalCredits = 0 -> GPA is 0 ; GPA is TotalGradePoints / TotalCredits).
+
+# GPA Calculation for Semester 2
+gpa(StudentID, 2, GPA) :-
+    write('Calculating GPA for Semester 2 for Student ID: '), write(StudentID), nl,
+    findall(Credits * GradePoints,
+            (module_details(Module, Year, 2, StudentID, GradePoints),
+             module_master(Module, Credits)),
+            CreditGradePointList),
+
+    % Display the CreditGradePointList
+    write('CreditGradePointList: '), write(CreditGradePointList), nl,
+
+    % Calculate the total grade points for Semester 2
+    sum_grade_points(CreditGradePointList, TotalGradePoints),
+
+    % Calculate the total credits for Semester 2
+    sum_credits(2, StudentID, TotalCredits),
+
+    % Display TotalGradePoints and TotalCredits
+    write('TotalGradePoints: '), write(TotalGradePoints), nl,
+    write('TotalCredits: '), write(TotalCredits), nl,
+
+    % Calculate GPA for Semester 2 as the ratio of TotalGradePoints to TotalCredits
+    (TotalCredits = 0 -> GPA is 0 ; GPA is TotalGradePoints / TotalCredits).
+
+# Predicate to sum grade points
+sum_grade_points([], 0).
+sum_grade_points([GradePoints | Rest], Total) :-
+    sum_grade_points(Rest, Subtotal),
+    Total is Subtotal + GradePoints.
+
+# Predicate to sum credits for a specific semester and student
+sum_credits(Semester, StudentID, TotalCredits) :-
+    findall(Credits,
+            (module_details(Module, Year, Semester, StudentID, _),
+             module_master(Module, Credits)),
+            CreditsList),
+    sum_list(CreditsList, TotalCredits).
+
+# Predicate to calculate cumulative GPA for a student
+cumulative_gpa(StudentID, CumulativeGPA) :-
+    % Calculate total grade points for Semester 1
+    gpa(StudentID, 1, GPA1),
+
+    # Calculate total grade points for Semester 2
+    gpa(StudentID, 2, GPA2),
+
+    # Find the TotalCredits for both semesters
+    sum_credits(1, StudentID, TotalCredits1),
+    sum_credits(2, StudentID, TotalCredits2),
+
+    # Calculate Cumulative GPA as the sum of total grade points for both semesters divided by the total credits
+    CumulativeGPA is (GPA1 * TotalCredits1 + GPA2 * TotalCredits2) / (TotalCredits1 + TotalCredits2).
--- ai_expert_system/prolog_controller.py
+++ ai_expert_system/prolog_controller.py
@@ -5,9 +5,100 @@ prolog.consult("prolog/knowledge_base.pl")
 
 print(prolog.query(list("parent_of('Mark',_)")))
 
-def generate_gpa():
-    gpa = [1]
-    return gpa
+
+def send_data_to_prolog():
+    try:
+        # Query the database to retrieve module details (grade points)
+        query = "SELECT module, student_id, academic_year, semester, grade_points FROM student_progress"
+        cursor.execute(query)
+        student_progress = cursor.fetchall()
+
+        # Iterate through the module details and assert them as facts in Prolog
+        for student_progress in student_progresses:
+            module, student_id, academic_year, semester, grade_points = student_progress
+            prolog.assertz(f"student_progress('{module}', {student_id},{academic_year},{semester}, {grade_points})")
+            print(f"Asserted: student_progress({module}', {student_id},{academic_year},{semester}, {grade_points})")
+
+        # Query the database to retrieve module master data (credits)
+        query = "SELECT module, no_of_credits FROM module"
+        cursor.execute(query)
+        module_master_data = cursor.fetchall()
+
+        # Iterate through module master data and assert them as facts in Prolog
+        for module_master_entry in module_master_data:
+            module, num_credits = module_master_entry
+            prolog.assertz(f"module_master('{module}', {no_of_credits})")
+            print(f"Asserted: module_master('{module}', {no_of_credits})")
+
+        print("Data sent to Prolog for calculation.")
+    except mysql.connector.Error as e:
+        print("Error sending data to Prolog:", e)
 
 
+def calculate_semester_gpa(student_id, semester):
+    try:
+        result = list(prolog.query(f"gpa({student_id}, {semester}, GPA)"))
+        if result:
+            gpa = result[0]["GPA"]
+            print(f"GPA for Semester {semester}: {gpa}")  # Print the GPA value
+            return gpa
+        else:
+            return None
+    except Exception as e:
+        print(f"Error calculating GPA for Semester {semester}: {e}")
+        return None
 
+
+# Calculate the cumulative GPA for a given student
+def calculate_cumulative_gpa(student_id):
+    try:
+        result = list(prolog.query(f'cumulative_gpa({student_id}, GPA)'))
+        if result:
+            cumulative_gpa = result[0]['GPA']
+            print(f"Cumulative GPA for Student {student_id}: {cumulative_gpa}")  # Print the Cumulative GPA value
+            return cumulative_gpa
+        else:
+            return None
+    except Exception as e:
+        print(f"Error calculating cumulative GPA for Student {student_id}: {e}")
+        return None
+
+
+# Generate the report
+def generate_report():
+    try:
+        # Send data to Prolog when the program starts
+        send_data_to_prolog()
+
+        # Query the database to retrieve student IDs
+        query = f"SELECT DISTINCT student_id FROM student_progress WHERE year = {academic_year}"
+        cursor.execute(query)
+        students = cursor.fetchall()
+
+        # Iterate through the student IDs and calculate the GPAs
+        for student_id in students:
+            student_id = student_id[0]
+            query = f"SELECT * FROM student WHERE student_id = {student_id}"
+            cursor.execute(query)
+            student_info = cursor.fetchone()
+
+            # Retrieve the student name from the student_master table
+            if student_info:
+                student_name = student_info[1]
+
+                # Retrieve GPAs from Prolog
+                gpa_semester_1 = calculate_semester_gpa(student_id, 1)
+                gpa_semester_2 = calculate_semester_gpa(student_id, 2)
+                cumulative_gpa = calculate_cumulative_gpa(student_id)
+
+                if gpa_semester_1 is not None and gpa_semester_2 is not None and cumulative_gpa is not None:
+                    result_text.insert(tk.END,
+                                       f"{student_id}\t{student_name}\t{gpa_semester_1:.2f}\t{gpa_semester_2:.2f}\t{cumulative_gpa:.2f}\n")
+                else:
+                    result_text.insert(tk.END, f"Error calculating GPA for Student {student_id}\n")
+    except ValueError:
+        print("Invalid input. Please enter a valid year and target GPA.\n")
+    except mysql.connector.Error as e:
+        print(f"Error querying the database: {e}\n")
+    except Exception as e:
+        print(f"An error occurred in Prolog: {e}\n")
